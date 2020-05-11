6b2acf3250841067d51aba47519c710ed8d84574
Shirysan <270081305@qq.com>
2020-05-11 11:20:19 +0800

    2020.5.9

---
+++ src/main/java/com/caopan/TrainSys/constant/VideoConver.java
@@ -0,0 +1,16 @@
+package com.caopan.TrainSys.constant;
+
+public class VideoConver {
+
+    public static final String ffmpegpath = "C://Users/Administrator/Desktop/list1/ffmpeg-20180714-c5329d6-win64-static/ffmpeg-20180714-c5329d6-win64-static/bin/ffmpeg.exe";		// ffmpeg工具安装位置
+    public static final String mencoderpath = "D:/ffmpeg/bin/mencoder"; 	// mencoder工具安装的位置
+
+    public static final String videofolder = "C://Users/Administrator/Desktop/list1/temp/"; 	// 需要被转换格式的视频目录
+    public static final String videoRealPath = "C://Users/Administrator/Desktop/list1/temp/"; 	// 需要被截图的视频目录
+
+    public static final String targetfolder = "C://Users/Administrator/Desktop/list1/finshvideo/"; // 转码后视频保存的目录
+    public static final String targetfolder2 = "C://Users/Administrator/Desktop/list1/finshprintvideo/"; // 水印后视频保存的目录
+    public static final String imageRealPath = "C://Users/Administrator/Desktop/list1/finshvideo/"; // 截图的存放目录
+    public static final String imageRealPath2 = "C\\\\:/Users/Administrator/Desktop/list1/logo/logo.jpg"; // 水印的存放目录
+
+}
